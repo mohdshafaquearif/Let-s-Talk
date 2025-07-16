fcf9c375b81968d9ce1f1ec48dbb8c0c5591687d
Mohd. Shafaque Arif <mohd.shafaquearif@Mohds-MacBook-Air.local>
2025-07-17 00:40:35 +0530

    Changing url to frontend deployed url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://let-s-talk.onrender.com/api" : "/api",
+  baseURL: "https://let-s-talk.onrender.com/api",
   withCredentials: true,
 });
