9b6517a6eaea2d4abb902e8ab21403ec0b39e3b3
Mohammad Shafaque Arif <97733931+mohdshafaquearif@users.noreply.github.com>
2025-08-15 01:41:19 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -13,7 +13,7 @@ const userSocketMap = {};
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173", "https://letstalkchat.netlify.app"], // local + production
-    methods: ["GET", "POST"],
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
     credentials: true,
   },
 });
